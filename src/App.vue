c5dfba350f0d7e11e10e67292d8ffcd46c123e88
FranciscoKloganB <francisco.teixeira.de.barros@outlook.com>
2021-11-09 22:08:29 +0000

    chore: minor typo fixes and test https preview

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-// https://github.com/vueuse/head
-// you can use this to manipulate the document head in any components,
-// they will be rendered correctly in the html results with vite-ssg
+/** 
+ * https://github.com/vueuse/head 
+ * Allows you to manipulate the document head in any components they will be
+ * rendered correctly in the html results with vite-ssg
+ */
 useHead({
-  title: 'Vitesse',
+  title: 'Vitesse Enterprise Starter',
   meta: [
     { name: 'description', content: 'Opinionated Vite Starter Template' },
   ],
