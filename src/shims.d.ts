6686e273748476be624ed034482a9a0a19d255e4
FranciscoKloganB <francisco.teixeira.de.barros@outlook.com>
2022-01-10 22:49:22 +0000

    chore: keep up with main repository

--- src/shims.d.ts
+++ src/shims.d.ts
@@ -7,7 +7,12 @@ declare interface Window {}
  * With vite-plugin-md, markdowns can be treat as Vue components
  */
 declare module '*.md' {
-  import { ComponentOptions } from 'vue'
+  import type { ComponentOptions } from 'vue'
   const component: ComponentOptions
   export default component
+}
+
+declare module '*.vue' {
+  import Vue from 'vue'
+  export default Vue
 }
