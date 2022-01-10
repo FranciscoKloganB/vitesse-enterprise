dba4316956d426ddc44a75f1223c7e0c66ceb3bf
FranciscoKloganB <francisco.teixeira.de.barros@outlook.com>
2022-01-10 23:26:31 +0000

    chore: use deprecated nodeVersion opt in cypress cfg

--- tests/common/cypress/plugins/index.ts
+++ tests/common/cypress/plugins/index.ts
@@ -1,7 +1,9 @@
 import { startDevServer } from '@cypress/vite-dev-server'
 
-export default function (on, config) {
-  on('dev-server:start', async (options) => startDevServer({ options }))
-
+export default function (
+  on: Cypress.PluginEvents,
+  config: Cypress.PluginConfigOptions
+): void | Cypress.ConfigOptions | Promise<Cypress.ConfigOptions> {
+  on('dev-server:start', async (options: Cypress.DevServerConfig) => startDevServer({ options }))
   return config
 }
