d7c689009d71dfaf334152e62b1986f9ef6968f3
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2022-02-23 22:09:28 +0000

    chore: add tiny component with counter from vueuse/core

---
+++ src/core/components/TheCounter.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    {{ count }}
+    <button class="inc" @click="inc()"> + </button>
+    <button class="dec" @click="dec()"> - </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  initial: number
+}>()
+const { count, inc, dec } = useCounter(props.initial)
+</script>
