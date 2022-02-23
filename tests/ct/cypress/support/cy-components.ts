2f7e17bfc5738728585c6f6afddeb7504a681913
FranciscoKloganB <francisco.teixeira.de.barros@outlook.com>
2022-02-23 14:08:22 +0000

    chore: add cypress type references on command declaration files

--- tests/ct/cypress/support/cy-components.ts
+++ tests/ct/cypress/support/cy-components.ts
@@ -1,7 +1,10 @@
+/// <reference types="cypress" />
+
 declare namespace Cypress {
   interface Chainable {
     /**
      * Mounts a component with access to a Pinia Store
+     *
      * @example cy.mountWithPinia(TheNavbar)
      * @example cy.mountWithPinia(TheNavbar, { propsData: { msg: 'hello world' } })
      */
