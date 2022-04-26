8a53b2bfcc2e378ccae76c728fdf508b3922d35f
FranciscoKloganB <francisco.teixeira.de.barros@outlook.com>
2022-04-26 20:15:26 +0100

    refactor: use pascal case

--- src/core/layouts/NotFoundLayout.vue
+++ src/core/layouts/NotFoundLayout.vue
@@ -10,7 +10,7 @@ const { t } = useI18n()
         <carbon-warning class="inline-block" />
       </p>
     </div>
-    <router-view />
+    <RouterView />
     <div>
       <button class="btn m-3 text-sm mt-8" @click="router.back()">
         {{ t('button.back') }}
