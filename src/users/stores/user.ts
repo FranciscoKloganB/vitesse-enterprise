fd0cd41f54e0011c6cb67eff1acf140cfee3d566
FranciscoKloganB <francisco.teixeira.de.barros@outlook.com>
2022-06-10 10:48:47 +0100

    style: fix eslint warning

--- src/users/stores/user.ts
+++ src/users/stores/user.ts
@@ -17,7 +17,9 @@ export const useUserStore = defineStore('user', () => {
    * @param name - new name to set
    */
   function setNewName(name: string) {
-    if (savedName.value) previousNames.value.add(savedName.value)
+    if (savedName.value) {
+      previousNames.value.add(savedName.value)
+    }
 
     savedName.value = name
   }
