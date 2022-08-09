a1a81f0a2e6f8c1a29aa2b3929f7b82bebee98fc
FranciscoKloganB <francisco.teixeira.de.barros@outlook.com>
2022-08-09 22:17:25 +0100

    fix(basic-smoke): wait for input to be enabled before typing

--- cypress/e2e/basic-smoke.cy.ts
+++ cypress/e2e/basic-smoke.cy.ts
@@ -9,6 +9,7 @@ context('Basic', () => {
     cy.contains('[Home Layout]').should('exist')
 
     cy.get('#input')
+      .should('be.enabled')
       .type('john_doe{Enter}')
       .url()
       .should('eq', 'http://127.0.0.1:4000/users/john_doe')
